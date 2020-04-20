d7b85b1e072756c5e7ffa1c230b6f6c406ad543c
zhen.an <zhen.an@zilliz.com>
2020-04-20 16:12:20 +0800

    part3

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,10 +70,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        // background_color: `#663399`,
-        // theme_color: `#329ef7`,
         display: `minimal-ui`,
-        icon: `src/images/favicon.ico` // This path is relative to the root of the site.
+        icon: `src/images/favicon.png` // This path is relative to the root of the site.
       }
     },
     {
