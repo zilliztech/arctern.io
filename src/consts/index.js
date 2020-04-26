9c1d742810b71ba892562067b11e962deb8727db
AzAz <az15921869329@gmail.com>
2020-04-26 22:07:12 +0800

    update

--- src/consts/index.js
+++ src/consts/index.js
@@ -1,14 +1,12 @@
 export const blog_site_cn = `http://zilliz.blog.csdn.net`;
 export const blog_site_en = `https://medium.com/@milvusio`;
 
-export const link_quick_start =
-  "docs/versions/v0.1.x/arctern-webdocs/development-doc-cn/html/python/quick_start/quick-start-pandas.html";
-export const link_overview =
-  "docs/versions/v0.1.x/arctern-webdocs/development-doc-cn/html/index.html";
+const Base_doc = `docs/versions`;
+export const link_quick_start = `${Base_doc}/v0.1.x/arctern-webdocs/development-doc-cn/html/python/quick_start/quick-start-pandas.html`;
+export const link_overview = `${Base_doc}/v0.1.x/arctern-webdocs/development-doc-cn/html/index.html`;
 
 export const link_mobi_blog = "/blogs/2019-08-26-vector-search-million.md";
-export const link_mobi_doc =
-  "docs/versions/v0.1.x/arctern-webdocs/development-doc-cn/html/index.html";
+export const link_mobi_doc = `${Base_doc}/versions/v0.1.x/arctern-webdocs/development-doc-cn/html/index.html`;
 export const link_join_slack =
   "https://join.slack.com/t/arctern-io/shared_invite/zt-e0yxka0h-RIo1spnKyLQhdwfWwygjYA";
 export const link_zhihu = "https://zhuanlan.zhihu.com/arctern";
