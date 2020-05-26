2f7c95c3eb72dd5c7e6b8eafc220b1b3875eb802
zhen.an <zhen.an@zilliz.com>
2020-05-26 18:06:42 +0800

    change doc site

--- src/consts/index.js
+++ src/consts/index.js
@@ -1,12 +1,12 @@
 export const link_root = `https://arctern.io/`;
 export const blog_site_cn = `http://zilliz.blog.csdn.net`;
 export const blog_site_en = `https://medium.com/@milvusio`;
-
+export const version = `v0.2.x`;
 const Base_doc = `docs/versions/`;
-export const link_quick_start_cn = `${link_root}${Base_doc}v0.1.x/arctern-webdocs/development-doc-cn/html/python/quick_start/quick-start-pandas.html`;
-export const link_quick_start_en = `${link_root}${Base_doc}v0.1.x/arctern-webdocs/development-doc-en/html/python/quick_start/quick-start-pandas.html`;
-export const link_overview_cn = `${Base_doc}v0.1.x/arctern-webdocs/development-doc-cn/html/index.html`;
-export const link_overview_en = `${Base_doc}v0.1.x/arctern-webdocs/development-doc-en/html/index.html`;
+export const link_quick_start_cn = `${link_root}${Base_doc}${version}/arctern-webdocs/development-doc-cn/html/python/quick_start/quick-start-pandas.html`;
+export const link_quick_start_en = `${link_root}${Base_doc}${version}/arctern-webdocs/development-doc-en/html/python/quick_start/quick-start-pandas.html`;
+export const link_overview_cn = `${Base_doc}${version}/arctern-webdocs/development-doc-cn/html/index.html`;
+export const link_overview_en = `${Base_doc}${version}/arctern-webdocs/development-doc-en/html/index.html`;
 
 export const link_join_slack =
   "https://join.slack.com/t/arctern-io/shared_invite/zt-e0yxka0h-RIo1spnKyLQhdwfWwygjYA";
